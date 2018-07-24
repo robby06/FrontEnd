ae948f690f42c37575c4bcdb9bbe5a3d0e0efbbf
robby06 <40376728+robby06@users.noreply.github.com>
2018-07-23 19:05:43 -0700

    Using wscat command to test out the application

---
+++ Real Time Data/Web Sockets real time/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+//reading a file with the fs module
+var fs = require('fs');
+//using custom module
+var extract = require('./extract');
+//import WS server module
+var wss = require('./websockets-server');
+
+//error handling
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+    };
+
+var server = http.createServer(function (req, res) {
+console.log('Responding to a request.');
+var filePath = extract(req.url);
+fs.readFile(filePath, function(err,data){
+    if (err) {
+        handleError(err, res);
+        return;
+        } else {
+        res.end(data);
+        }
+});
+});
+server.listen(3000);
